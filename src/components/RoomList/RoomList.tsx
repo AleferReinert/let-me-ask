0f38c6a4508e5fef23f71efcb8b20b6b49465a3e
Alefer Reinert <aleferreinert@gmail.com>
2025-07-09 17:00:06 -0300

    fix(RoomList): responsive skeleton

--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -20,15 +20,17 @@ export function RoomList() {
 
   return (
     <ul className="space-y-2">
-      {isLoading &&
+      {!isLoading &&
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
           <li
             key={item}
-            className="p-4 bg-white text-zinc-800 flex items-center gap-4 rounded-lg"
+            className="p-4 bg-white text-zinc-800 flex flex-col sm:flex-row gap-2 sm:items-center sm:gap-4 rounded-lg"
           >
             <div className="w-40 h-7 bg-zinc-200 animate-pulse rounded-md" />
-            <div className="w-[88px] h-7 bg-zinc-200 animate-pulse rounded-[50px]" />
-            <div className="w-[72px] h-5 bg-zinc-200 animate-pulse rounded-sm ml-auto" />
+            <div className="flex flex-1 items-center">
+              <div className="w-[88px] h-7 bg-zinc-200 animate-pulse rounded-[50px]" />
+              <div className="w-[72px] h-5 bg-zinc-200 animate-pulse rounded-sm ml-auto" />
+            </div>
           </li>
         ))}
 
