93967a65d72092320202cd7e3094374fbc860f3e
Alefer Reinert <aleferreinert@gmail.com>
2025-07-12 17:19:00 -0300

    feat(Alert,QuestionList): add alert on question list and change padding

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -2,28 +2,28 @@ import type { ComponentProps } from 'react'
 import { tv } from 'tailwind-variants'
 
 export interface AlertProps extends ComponentProps<'div'> {
-  variant?: 'error' | 'success' | 'info' | 'warning'
-  children: React.ReactNode
+	variant?: 'error' | 'success' | 'info' | 'warning'
+	children: React.ReactNode
 }
 
 const alert = tv({
-  base: 'flex py-2 px-4 rounded-lg',
-  variants: {
-    variant: {
-      error: 'text-red-900 bg-red-100',
-      success: 'text-green-900 bg-green-100',
-      info: 'text-sky-900 bg-sky-100',
-      warning: 'text-amber-900 bg-amber-100'
-    }
-  }
+	base: 'flex p-4 rounded-lg',
+	variants: {
+		variant: {
+			error: 'text-red-900 bg-red-100',
+			success: 'text-green-900 bg-green-100',
+			info: 'text-sky-900 bg-sky-100',
+			warning: 'text-amber-900 bg-amber-100'
+		}
+	}
 })
 
 export const Alert = ({ variant = 'error', children, className, ...rest }: AlertProps) => {
-  const classNames = alert({ variant, className })
+	const classNames = alert({ variant, className })
 
-  return (
-    <div role="alert" className={classNames} {...rest}>
-      {children}
-    </div>
-  )
+	return (
+		<div role='alert' className={classNames} {...rest}>
+			{children}
+		</div>
+	)
 }
--- src/components/QuestionList/QuestionList.tsx
+++ src/components/QuestionList/QuestionList.tsx
@@ -1,28 +1,29 @@
 import { useRoomQuestions } from '../../http/useRoomQuestions'
+import { Alert } from '../Alert/Alert'
 import { QuestionAnswer } from '../QuestionAnswer/QuestionAnswer'
 
 interface QuestionListProps {
-  roomId: string
+	roomId: string
 }
 
 export function QuestionList({ roomId }: QuestionListProps) {
-  const { data, isLoading } = useRoomQuestions(roomId)
+	const { data, isLoading } = useRoomQuestions(roomId)
 
-  if (isLoading) {
-    return <p>Carregando...</p>
-  }
+	if (isLoading) {
+		return <p>Carregando...</p>
+	}
 
-  if (!data || data?.length === 0) {
-    return <p className="text-zinc-500">Ainda não há perguntas nesta sala.</p>
-  }
+	if (!data || data?.length === 0) {
+		return <Alert variant='info'>Ainda não há perguntas nesta sala.</Alert>
+	}
 
-  return (
-    <ul className="space-y-2">
-      {data?.map((question) => (
-        <li key={question.id}>
-          <QuestionAnswer {...question} />
-        </li>
-      ))}
-    </ul>
-  )
+	return (
+		<ul className='space-y-2'>
+			{data?.map(question => (
+				<li key={question.id}>
+					<QuestionAnswer {...question} />
+				</li>
+			))}
+		</ul>
+	)
 }
