563eff826daa7b95d6ce178f73cd3af06b2c7da3
Alefer Reinert <aleferreinert@gmail.com>
2025-07-13 15:10:18 -0300

    style(Field): change border color on focus

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -9,7 +9,7 @@ interface FieldProps extends ComponentProps<'input'> {
 }
 
 const field = tv({
-	base: 'leading-normal bg-white rounded-lg border text-zinc-800 px-4 w-full transition focus:outline-none focus:border-zinc-800 disabled:cursor-not-allowed disabled:opacity-85',
+	base: 'leading-normal bg-white rounded-lg border text-zinc-800 px-4 w-full transition focus:outline-none focus:border-theme-primary-500 disabled:cursor-not-allowed disabled:opacity-85',
 	variants: {
 		errorMessage: {
 			true: 'border-red-700',
