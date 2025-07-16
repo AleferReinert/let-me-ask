6b6ccfbcd3cadbb5fe13c4ad837892421f439907
Alefer Reinert <aleferreinert@gmail.com>
2025-07-16 19:39:42 -0300

    feat(RoomItem): change design and add button

--- src/components/RoomItem/RoomItem.tsx
+++ src/components/RoomItem/RoomItem.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom'
 import { dayjs } from '../../lib/dayjs'
 import { Badge } from '../Badge/Badge'
+import { Button } from '../Button/Button'
 
 export interface RoomItemProps {
 	id: string
@@ -12,20 +12,24 @@ export interface RoomItemProps {
 
 export function RoomItem({ id, name, description, questionsCount, createdAt }: RoomItemProps) {
 	return (
-		<Link
-			data-testid='RoomItemComponent'
-			to={`/room/${id}`}
-			title='Entrar'
-			className='p-4 bg-white flex flex-col gap-2 rounded-lg transition group hover:bg-theme-primary-500'
-		>
-			<h3 className='font-medium text-zinc-800 flex gap-4 text-lg group-hover:text-white'>
-				{name}
-				<Badge questionsCount={questionsCount} />
-			</h3>
-			<p className='text-sm text-zinc-500 group-hover:text-white'>{description}</p>
-			<div className='flex items-center flex-1'>
-				<p className='ml-auto text-zinc-500 text-sm transition group-hover:text-white'>{dayjs(createdAt).fromNow()}</p>
+		<div data-testid='RoomItemComponent' title='Entrar' className='p-4 bg-white'>
+			<div className='flex flex-col gap-2 mb-2'>
+				<h3 className='font-heading text-zinc-800 flex gap-4 text-lg group-hover:text-white'>
+					{name}
+					<Badge questionsCount={questionsCount} />
+				</h3>
 			</div>
-		</Link>
+			<div className='flex justify-between items-center gap-4'>
+				<div>
+					<p className='text-sm text-zinc-500 group-hover:text-white'>{description}</p>
+					<p className='ml-auto text-zinc-500 w-full text-sm transition group-hover:text-white'>
+						{dayjs(createdAt).fromNow()}
+					</p>
+				</div>
+				<Button as='link' to={`/room/${id}`}>
+					Entrar
+				</Button>
+			</div>
+		</div>
 	)
 }
